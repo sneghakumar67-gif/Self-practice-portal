896791cbf10302f43370e9f4f432b9b1ffcac6c4
sneghakumar67-gif <sneghakumar67@gmail.com>
2025-10-02 15:08:20 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$host = "https://warm-pithivier-774dc9.netlify.app/";
+$host = "110.172.151.103";
 $user = "root";
 $pass = ""; // keep empty for XAMPP unless you set a password
 $dbname = "newportal";  // <-- Correct database name
@@ -10,6 +10,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
+
 
 
 
